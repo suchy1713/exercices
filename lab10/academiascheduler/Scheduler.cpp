506a02e6e23219ef7861558400b9380277df1ebb
WojciechDolata <wojciech.k.dolata@gmail.com>
2018-05-17 18:28:25 +0200

    Schedule test działa

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -48,22 +48,50 @@ const academia::SchedulingItem &academia::Schedule::operator[](int i) const{
     }
 }
 
-Schedule Schedule::OfTeacher(int) const {
-    return Schedule();
+Schedule Schedule::OfTeacher(int teacher) const {
+    Schedule returnable;
+    for(const auto &current : items) {
+        if(current.TeacherId() == teacher) {
+            returnable.InsertScheduleItem(current);
+        }
+    }
+    return returnable;
 }
 
-Schedule Schedule::OfRoom(int) const {
-    return Schedule();
+Schedule Schedule::OfRoom(int room) const {
+    Schedule returnable;
+    for(const auto &current : items) {
+        if(current.RoomId() == room) {
+            returnable.InsertScheduleItem(current);
+        }
+    }
+    return returnable;
 }
 
-Schedule Schedule::OfYear(int) const {
-    return Schedule();
+Schedule Schedule::OfYear(int year) const {
+    Schedule returnable;
+    for(const auto &current : items) {
+        if(current.Year() == year) {
+            returnable.InsertScheduleItem(current);
+        }
+    }
+    return returnable;
 }
 
 std::vector<int> Schedule::AvailableTimeSlots(int n_time_slots) const {
-    return std::vector<int>();
+    std::vector <int> returnable;
+    int tab[n_time_slots+1];
+    for (const auto &current : items) {
+        tab[current.TimeSlot()]=-1;
+    }
+    for (int i=1; i<n_time_slots+1; i++) {
+        if(tab[i]!=-1) {
+            returnable.push_back(i);
+        }
+    }
+    return returnable;
 }
 
 size_t Schedule::Size() const {
-    return 0;
+    return items.size();
 }
