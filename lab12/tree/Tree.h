5966a79b0b07a4e79bc1a7218333e64438863a8b
Piotr Wawrzynów <wawrzynow.piotr@gmail.com>
2018-06-03 21:58:20 +0200

    insert rekurencyjnie ale coś dalej nie działa

--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -6,90 +6,140 @@
 #define JIMP_EXERCISES_TREE_H
 
 #include <memory>
-
+#include <algorithm>
+#include <utility>
 
 namespace tree {
     template <typename T>
-    class Element {
+    class Node {
     public:
-        Element(T a) {val_ = a;};
-        ~Element() = default;
+        Node(T a) {val_ = a;};
+        Node(const Node<T>&);
+        Node(Node<T>&&);
+        Node &operator=(const Node<T>&);
+        Node &operator=(Node<T>&&);
+        ~Node() = default;
         T val_;
-        std::unique_ptr<Element<T>> left_ = nullptr;
-        std::unique_ptr<Element<T>> parent_ = nullptr;
-        std::unique_ptr<Element<T>> right_ = nullptr;
+        Node<T>* left_ = nullptr;
+        Node<T>* parent_ = nullptr;
+        Node<T>* right_ = nullptr;
     };
 
+    template <typename T>
+    Node<T>::Node(const Node<T>& x){
+        left_ = new Node<T>;
+        std::copy(left_, left_ + sizeof(Node<T>), x.left_);
+        right_ = new Node<T>;
+        std::copy(right_, right_ + sizeof(Node<T>), x.right_);
+        parent_ = new Node<T>;
+        std::copy(parent_, parent_ + sizeof(Node<T>), x.parent_);
+    }
+
+    template <typename T>
+    Node<T>::Node(Node<T>&& x){
+        std::swap(left_, x.left_);
+        std::swap(right_, x.right_);
+        std::swap(parent_, x.parent_);
+    }
+
+    template <typename T>
+    Node<T> & Node<T>::operator=(const Node<T>& x){
+        if (this == &x) {
+            return *this;
+        }
+
+        delete right_;
+        delete left_;
+        delete parent_;
+
+        left_ = new Node<T>;
+        std::copy(left_, left_ + sizeof(Node<T>), x.left_);
+        right_ = new Node<T>;
+        std::copy(right_, right_ + sizeof(Node<T>), x.right_);
+        parent_ = new Node<T>;
+        std::copy(parent_, parent_ + sizeof(Node<T>), x.parent_);
+    }
+
+    template <typename T>
+    Node<T> & Node<T>::operator=(Node<T>&& x){
+        if (this == &x) {
+            return x;
+        }
+
+        delete right_;
+        delete left_;
+        delete parent_;
+
+        left_ = nullptr;
+        right_ = nullptr;
+        parent_ = nullptr;
+        std::swap(left_, x.left_);
+        std::swap(right_, x.right_);
+        std::swap(parent_, x.parent_);
+    }
 
     template <typename T>
     class Tree {
     public:
         Tree() = default;
-        Tree(const T &val);
+        Tree(T val);
         ~Tree() = default;
-        void Insert (const Element<T> &e);
+        Node<T>* ActualInsert(Node<T>*, T, int);
+        void Insert (T e);
         T Value() const;
         int Size() const;
         int Depth() const;
 
     private:
-        std::unique_ptr<Element<T>> head_;
+        Node<T>* head_ = nullptr;
         int size_=0;
         int depth_=0;
     };
 
     template <typename T>
-    void Tree<T>::Insert(const Element<T> &e) {
-        Element<T> *current = head_.get();
-        while(current!= nullptr){
-            if(e.val_>current->val_) {
-                current = current->right_.get();
-            }
-            else if(e.val_<current->val_) {
-                current = current->left_.get();
-            }
-            else break;
+    Node<T>* Tree<T>::ActualInsert(Node<T>* curr, T e, int d) {
+        if(curr == nullptr){
+            Node<T> n = Node<T>(e);
+            depth_++;
+            return &n;
         }
-        current->val_ = e.val_;
 
-/*
-        if(e.val_>head_->val_) {
-            i = move(i->right_);
+        else if(e <= curr->val_){
+            curr->left_ = ActualInsert(curr->left_, e, ++d);
         }
 
         else{
-            i = move(i->left_);
+            curr->right_ = ActualInsert(curr->right_, e, ++d);
         }
 
-        do{
-            if(e.val_>i->val_) {
-                i = move(i->right_);
-            }
-
-            else{
-                i = move(i->left_);
-            }
-        }while(i != nullptr);
-
-
-        if(e.val_>i->val_) {
-            i->right_ = move(n);
+        if(d > depth_){
+            depth_++;
         }
 
-        else{
-            i->left_ = move(n);
-        }*/
-
-
+        return curr;
     }
+
     template <typename T>
-    Tree<T>::Tree(const T &val) {
-        Element<T> tmp(val);
-        std::unique_ptr<Element<T>> a = std::make_unique<Element<T>>(tmp);
-        head_ = move(a);
+    void Tree<T>::Insert(T e) {
+        if(head_ == nullptr){
+            head_ = ActualInsert(head_ ,e, 0);
+        }
+
+        else{
+            ActualInsert(head_ ,e, 0);
+        }
+
+        size_ += 1;
     }
+
+    template <typename T>
+    Tree<T>::Tree(T val) {
+        Insert(val);
+    }
+
     template <typename T>
     T Tree<T>::Value() const {
+        T x = head_->val_;
         return head_->val_;
     }
     template <typename T>
